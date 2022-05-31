7a9fdf16d0be3a94f25140733efbc97d3d5f8680
nihaltk <nihaltk982@gmail.com>
2022-05-31 10:40:52 +0300

    Updates

---
+++ precache-manifest.04e7d952e94bc10656fb51d7eff7f229.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0081913640d56de47d1",
+    "url": "/Ecommerce/static/css/main.d13b5a53.chunk.css"
+  },
+  {
+    "revision": "e0081913640d56de47d1",
+    "url": "/Ecommerce/static/js/main.e0081913.chunk.js"
+  },
+  {
+    "revision": "791806e070148d25b8a9",
+    "url": "/Ecommerce/static/css/1.4c756b93.chunk.css"
+  },
+  {
+    "revision": "791806e070148d25b8a9",
+    "url": "/Ecommerce/static/js/1.791806e0.chunk.js"
+  },
+  {
+    "revision": "b7749c7bf33cec22dfc4",
+    "url": "/Ecommerce/static/js/runtime~main.b7749c7b.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Ecommerce/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Ecommerce/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Ecommerce/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Ecommerce/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "5b89f34d2417b3d5e90111486cd64a3d",
+    "url": "/Ecommerce/static/media/Logo.5b89f34d.png"
+  },
+  {
+    "revision": "954a761a71202cde4ea369bf7d5f6b42",
+    "url": "/Ecommerce/static/media/slide-2.954a761a.png"
+  },
+  {
+    "revision": "5fb05c09d59a2e84a0f91b8745194ef6",
+    "url": "/Ecommerce/static/media/1.5fb05c09.jpg"
+  },
+  {
+    "revision": "dc9d8bde63d9bb8fe68f4e7827400fca",
+    "url": "/Ecommerce/static/media/1.dc9d8bde.jpg"
+  },
+  {
+    "revision": "1016def4fd933bac850601e2878b5b48",
+    "url": "/Ecommerce/static/media/1.1016def4.jpg"
+  },
+  {
+    "revision": "fa80db9598172999cb809004de6ee07e",
+    "url": "/Ecommerce/static/media/1.fa80db95.jpg"
+  },
+  {
+    "revision": "6b06283780ae215700d9c64f3ee2308d",
+    "url": "/Ecommerce/static/media/1.6b062837.png"
+  },
+  {
+    "revision": "59a4daf9ad6739f58292449b1258f75b",
+    "url": "/Ecommerce/static/media/2.59a4daf9.png"
+  },
+  {
+    "revision": "b4e693e76609c8f24264ad74a98ac74e",
+    "url": "/Ecommerce/index.html"
+  }
+];
